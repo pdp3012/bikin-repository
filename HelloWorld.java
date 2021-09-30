ddb519d146ece93b7a9bd7ce0e95fb86cd4c0b64
unknown <pradiptapryanda301203@gmail.com>
2021-10-01 06:47:42 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    
+	public static void main(String[] args) {
+	    System.out.println("NAMA  : Pradipta Deska Pryanda"); 
+		System.out.println("NIM   : 6706213093");
+		System.out.println("Kelas : D3IF 45-04");
+	}
+}	
